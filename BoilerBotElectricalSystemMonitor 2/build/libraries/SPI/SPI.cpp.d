7e8b92c180740474948b873fc45fb71badfd0c41
ShittyRobot <01001010kevin@gmail.com>
2019-07-17 22:56:44 -0700

    added some circuit and updated old ones

---
+++ BoilerBotElectricalSystemMonitor 2/build/libraries/SPI/SPI.cpp.d
@@ -0,0 +1,23 @@
+/Users/ChengMing/Documents/Robomaster/Power-System/BoilerBotElectricalSystemMonitor/build/libraries/SPI/SPI.cpp.o: \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/libraries/SPI/src/SPI.cpp \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/libraries/SPI/src/SPI.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/stdlib.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/machine/ieeefp.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/_ansi.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/newlib.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/config.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/features.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/esp32/xtensa/config/core-isa.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/reent.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/_ansi.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/_types.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/machine/_types.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/machine/_default_types.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/lock.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/cdefs.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/machine/stdlib.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/alloca.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/variants/feather_esp32/pins_arduino.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/stdint.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/sdk/include/newlib/sys/_intsup.h \
+ /Users/ChengMing/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/cores/esp32/esp32-hal-spi.h
